f1d680202ee02da2e7a0cd88056fc8aadbeff416
Sean Wong <derpydm@gmail.com>
2021-07-27 14:29:04 +0800

    Add News page with basic modules

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -9,6 +9,7 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import * as styles from "./styles/apps.module.scss";
+import * as newsStyles from "./styles/news.module.scss";
 
 function StudentsPage() {
 
@@ -16,14 +17,25 @@ function StudentsPage() {
         <Header />
         <main className={styles.page} style={{ backgroundImage: "url(/static/assets/genericBg.svg)" }}>
             <section className={[styles.section, styles.sectionOne].join(" ")}>
-                <h1>Our Apps</h1>
-                {/*<div className={styles.stats}>   
-                    <div>
-                        <h1>33</h1>
-                        <h3>Apps Published</h3>
+                <h1>News</h1>
+                <div className={newsStyles.flexContainer}>
+                    <div className={newsStyles.articleBlock}>
+                            <StaticImage src="../images/banners/appBanner.png" className={newsStyles.articleImgContainer} alt='news article'/>                      
+                        <div className={newsStyles.articleText}>
+                            <h2 className={newsStyles.articleTitle}>Apple CEO Tim Cook visited us in 2019, and encouraged students to code</h2>
+                            <a href="#" className={newsStyles.seeMoreButton}>SEE MORE</a>
+                        </div>
                     </div>
-                </div>*/}
-                <ReadMore />
+                    <div className={newsStyles.articleBlock}>
+                            <StaticImage src="../images/speakers/yuma.png" className={newsStyles.articleImgContainer} alt='news article'/>                      
+                        <div className={newsStyles.articleText}>
+                            <h2 className={newsStyles.articleTitle}>Test Headline 2</h2>
+                            <a href="#" className={newsStyles.seeMoreButton}>SEE MORE</a>
+                        </div>
+                    </div>
+                </div>
+
+                
             </section>
             <Footer />
         </main>
