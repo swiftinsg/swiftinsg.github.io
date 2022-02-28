120e17ba21700ce39e7a9f3230e32b8e268e9e90
James Chen <unknownguy2002@gmail.com>
2022-02-28 12:25:28 +0800

    Update application status

--- src/pages/joinus.tsx
+++ src/pages/joinus.tsx
@@ -55,14 +55,15 @@ function StudentsPage() {
         <Page className={styles.page}>
             <BannerSection bg={joinusBanner}>
                 <h1>Join Us</h1>
-                <h3 id={styles.bodyText}>Applications for the Class of 2021 are closed.</h3>
-                <h3 id={styles.bodyText}>Email <a className={styles.linkText} href="mailto:hello@swiftinsg.org">hello@swiftinsg.org</a> to find out more, or follow us on social media to keep up with the Class of 2021.</h3>
+                <h3 id={styles.bodyText}>Applications for the Class of 2022 are open!</h3>
+                {/* <h3 id={styles.bodyText}>Email <a className={styles.linkText} href="mailto:hello@swiftinsg.org">hello@swiftinsg.org</a> to find out more, or follow us on social media to keep up with the Class of 2021.</h3> */}
                 <div className={styles.flexContainer}>
                 <a href="https://www.instagram.com/swiftinsg" className={styles.flavorButton}>
                     <FontAwesomeIcon icon={["fab", "instagram"]} />Follow us on Instagram</a>
                 <a href="https://www.youtube.com/channel/UCGYAY8RMWAkx_aDkPV_tFsA" className={styles.flavorButton} id={styles.altButton}>
                     <FontAwesomeIcon icon={["fab", "youtube"]} />Explore our Youtube Channel</a>
                 </div>
+                
                 <ReadMore />
             </BannerSection>
             <Section className={styles.sectionTwo}>
