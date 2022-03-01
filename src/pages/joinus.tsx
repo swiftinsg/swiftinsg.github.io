d2d14c49b558bc2a115d965ff6eb420033cb35c7
James Chen <unknownguy2002@gmail.com>
2022-03-01 11:06:52 +0800

    Update programme outline

--- src/pages/joinus.tsx
+++ src/pages/joinus.tsx
@@ -15,38 +15,58 @@ import { Link } from "gatsby";
 
 let steps = [
     {
-        title: "Introduction to programming in Swift & Xcode",
-        desc: `We begin by introducing students to programming in the latest version of the Swift programming language. Swift is a modern, open-source language created by Apple, designed to be easy for beginners to learn, and powerful for advanced users to utilise in creating iOS apps.
-               Students will be introduced to coding concepts in Swift, using Xcode Playgrounds: constants, variables, data types, operators, and control flow.`,
+        title: "Introduction to programming in SwiftUI",
+        desc: `This module will introduce students to SwiftUI. SwiftUI is a new, powerful declarative UI framework for Apple’s platforms. It is designed to be easy to use and pick up for beginners compared to its predecessors, UIKit and AppKit. Students will be introduced to the concept of modifiers and stack views within SwiftUI. Students will get started on Swift Playgrounds.`,
         pic: "/assets/xcode.png"
     },
     {
-        title: "From Idea to launch",
-        desc: `This module introduces students to design thinking philosophies, and how to apply them to create, validate, and pitch ideas, how to find product-market fit, and do basic wireframing. We will include case studies on technology products well known ones, as well as our own, which are more obscure but rich in lessons learned.
-               On the design front, students will be introduced to user interface and user experience (UI/UX) design tools such as Figma to prototype their own apps.​
-               The second half of this module introduces further development in Xcode, such as functions, classes, collections, and further UIKit controls such as Stack Views.`
+        title: "Introduction to programming in Swift",
+        desc: `This module will introduce students to programming in the latest version of the Swift programming language. Swift is a modern, open-source language created by Apple, designed to be easy for beginners to learn, and powerful for advanced users to utilise in creating iOS apps.
+                Students will be introduced to coding concepts in Swift, using Xcode Playgrounds: constants, variables, data types, operators, and control flow. `
     },
     {
-        title: "Further iOS app development in Xcode",
-        desc: `This module begins exploring the development on iOS apps using Xcode. Building upon the foundation laid in Modules 1 and 2, students will be guided into creating a variety of apps with real-world applications, making full use of the iOS UIKit framework.
-        iOS concepts include critical UI elements such as table views, collection views, navigation controllers, and tab bar controllers. Advanced Swift language constructs such as Optionals, Guard, and enumerations will be introduced here.`
+        title: "State and Interactivity",
+        desc: `Students will learn to create interactive apps by using interactive elements, stored states, and animations.
+                They will learn to use @State variables, frames and positioning, more modifiers and animations. `
     },
     {
-        title: "Designing for delight",
-        desc: `Apple has always emphasised the need to delight users in creating apps, and regularly features apps that shine through in terms of design, experience, and animation. This module will provide some background for students on how to achieve the high standards of design expected of iOS apps.
-               This module brings an introduction to icon design and graphics tools, using, preferably, freely available tools such as Figma.
-               In Xcode, students will learn how to implement animations in Xcode, using basic UIView animations and transitions.`
+        title: "Design and Prototyping",
+        desc: `This module introduces students to design thinking philosophies, and how to apply them to create, validate, and pitch ideas, how to find product-market fit, and do basic wireframing. We will include case studies on technology products—well known ones, as well as our own, which are more obscure but rich in lessons learned. 
+                They will be introduced to Keynote for prototyping, resources such as Apple’s Human Interface Guidelines and accessibility guidelines.
+                On the design front, students will be introduced to user interface and user experience (UI/UX) design tools, preferably free ones such as XD or Figma, to prototype their own apps.`
     },
     {
-        title: "Persistence and libraries",
-        desc: `This module covers more technically advanced features often found in iOS apps, particularly permanent data storage (on device or in the cloud), and working with external web services to store and retrieve data.
-               Students will also begin to make full use of iOS device capabilities by learning and utilising powerful built-in iOS application programming interfaces (APIs). These include web and API access, geolocation, accelerometer readings, and more. Students will also be exposed to commonly-used external libraries that allow them to quickly implement powerful features while saving on development time.`
+        title: "Exploring Swift",
+        desc: `Students will learn intermediate-level Swift features, essential for building apps in SwiftUI such as arrays, structs, properties and methods. Students will also be introduced to closures.`
+    },
+    {
+        title: "Array-Based App",
+        desc: `Students will make use of their knowledge of arrays and structs to create a multi-screen array based app`
+    },
+    {
+        title: "Swift in-depth",
+        desc: `Students will learn about in-depth features of the Swift programming language which are commonly used in SwiftUI, namely, iteration and optionals.`
+    },
+    {
+        title: "List-Based Apps",
+        desc: `Students will learn to create an app that allows them to perform basic CRUD (Create, Read, Update, Delete) operations. Students will also learn more about manipulating arrays and navigation views in SwiftUI.`
+    },
+    {
+        title: "Persistence and Packages",
+        desc: `This module covers more technically advanced features often found in iOS apps, particularly permanent data storage (on device or in the cloud), and working with external web services to store and retrieve data. 
+                Students will also begin to make full use of iOS device capabilities by learning and utilising powerful built-in iOS application programming interfaces (APIs). These include web and API access, geolocation, accelerometer readings, and more. Students will also be exposed to commonly-used external libraries that allow them to quickly implement powerful features while saving on development time.`
+    },
+    {
+        title: "Introduction to UIKit",
+        desc: `This module covers UIKit and how students can integrate UIKit views into their SwiftUI apps. UIKit is an imperative UI framework developed by Apple for creating user interfaces for iOS/iPadOS devices. Currently, many technologies and packages, including Apple’s, rely on UIKit to work as it provides more flexibility than SwiftUI.
+                The module will ease students from SwiftUI’s declarative syntax to UIKit’s imperative syntax by introducing them to concepts like SwiftUI Path, a way to draw paths and create rich vectors imperatively, within SwiftUI, similar to Python Turtle. Following which, they will be introduced to UIView, UIViewRepresentable and Autolayout.
+                Importantly, they will be focusing on programmatic UIKit instead of Storyboards as programmatic UIKit has better interoperability with SwiftUI.`
     },
     {
         title: "Building, marketing, and launching your app",
-        desc: `Creating an app is never just about the technical requirements; companies and developers have to make an effort to get noticed in a store containing millions of other apps.
-               In this final module, students will revise their design thinking principles to ensure they don’t “make something nobody wants”, revenue models available to apps, how to project-manage apps, and pitch them to “investors” and customers.
-               Finally, students will be closely guided through the process of proposing, designing, implementing, and finally submitting their apps to the Apple App Store for public consumption. Students will also learn how best to present and tell a story about their apps.`
+        desc: `Creating an app is never just about the technical requirements; companies and developers have to make an effort to get noticed in a store containing millions of other apps. 
+                In this final module, students will revise their design thinking principles to ensure they don’t “make something nobody wants”, revenue models available to apps, how to project-manage apps, and pitch them to “investors” and customers. 
+                Finally, students will be closely guided through the process of proposing, designing, implementing, and finally submitting their apps to the Apple App Store for public consumption. Students will also learn how best to present and tell a story about their apps.`
     }
 ]
 
@@ -75,7 +95,10 @@ function StudentsPage() {
                     <ol>
                         <li>Take note of the important dates below for deadlines and briefings. </li>
                         <li>Confirm that you can attend the <b>vast majority</b> of the scheduled sessions. <Link to="https://cld.tk.sg/6quE6O6P">See the schedule below.</Link></li>
-                        <li>Try out Unit 1 of our <Link to="https://tk.sg/swiftui-videos-unit1">SwiftUI video series</Link>. You may use Swift Playgrounds on iPad or Mac for this. If you have any issues, email us at <Link to="mailto:hello@swiftinsg.org">hello@swiftinsg.org</Link>.</li>
+                        <li>Try out Unit 1 of our <Link to="https://tk.sg/swiftui-videos-unit1">SwiftUI video series</Link>. You may use <Link to="https://www.apple.com/swift/playgrounds/">Swift Playgrounds</Link> on iPad or Mac for this. If you have any issues, email us at <Link to="mailto:hello@swiftinsg.org">hello@swiftinsg.org</Link>.</li>
+                        <ul>
+                            <li>If you are using a managed iPad that is unable to build App projects or using Swift Playgrounds 4 for Mac, you can create a normal Playground and paste in the code from <Link to="https://tk.sg/swiftpgtemplate">https://tk.sg/swiftpgtemplate</Link>.</li>
+                        </ul>
                         <li>Prepare a <b>minute-long</b> video introducing yourself and your end-product based on [3]. Reflect on your experience creating it (how you got it to work, any challenges, anything new you've tried, etc.). Our focus will be on the content of the video and not the video production quality. However, please ensure the video and audio are clear enough that we are able to understand what you say.</li>
                         <li>Prepare any supporting documents, links or any information you think might be relevant when we consider your application</li>
                         <li>Complete an individual entrance test and survey. The test includes coding, design, essay questions (involving the aforementioned video), and should be completed in about an hour.</li>
