1231ba31b88f06ce8aeeaafac49ff759d00d30da
James Chen <unknownguy2002@gmail.com>
2022-03-04 21:21:23 +0800

    Update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ function Footer() {
 
     return (<>
         <footer className={styles.footer}>
-            <span>&copy; 2021 Swift Innovators' Summit. All rights reserved.</span>
+            <span>&copy; 2022 Swift Innovators' Summit. All rights reserved.</span>
             <div>
                 <a target="_blank" href="https://www.instagram.com/swiftinsg/"><FontAwesomeIcon icon={["fab", "instagram"]} /></a>
                 <a target="_blank" href="https://twitter.com/swiftinsg"><FontAwesomeIcon icon={["fab", "twitter"]} /></a>
