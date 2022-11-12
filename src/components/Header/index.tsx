e02f2283315ecf87a7f622955514e206e4b1c360
harish-f <harish_ram@s2021.ssts.edu.sg>
2022-11-13 01:25:33 +0800

    fixed text wrapping bug when mobile navbar opens

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "gatsby";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import * as styles from "./index.module.scss";
 
@@ -10,46 +10,73 @@ let links = [
   { href: "/students", name: "Students" },
   { href: "/apps", name: "Apps" },
   { href: "/news", name: "News & Achievements" },
-  { href: "/joinus", name: "Join Us" }
-]
+  { href: "/joinus", name: "Join Us" },
+];
 
 function Header({ startWhite }: { startWhite?: boolean }) {
+  const [isBeyondScroll, setBeyondScroll] = useState(false);
+  const [isDrawerOpen, setDrawerOpen] = useState(false);
 
-    const [isBeyondScroll, setBeyondScroll] = useState(false);
-    const [isDrawerOpen, setDrawerOpen] = useState(false);
+  useEffect(() => {
+    function onScroll(e) {
+      let scroll = window.scrollY;
+      setBeyondScroll(scroll > 25);
+    }
 
-    useEffect(() => {
+    window.addEventListener("scroll", onScroll);
 
-        function onScroll(e) {
-            let scroll = window.scrollY;
-            setBeyondScroll(scroll > 25);
-        }
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-        window.addEventListener('scroll', onScroll);
-
-        return () => window.removeEventListener('scroll', onScroll);
-    }, []);
-
-    return (<>
-        <header className={[styles.landingHeader, isBeyondScroll ? styles.beyondScroll : startWhite ? styles.startWhite : ""].join(" ")}>
-          <Link to="/"><div className={styles.swiftIcon} /></Link>
-          <div className={styles.headerLinks}>
-            {/*<div className={styles.dropDown}>
+  return (
+    <>
+      <header
+        className={[
+          styles.landingHeader,
+          isBeyondScroll
+            ? styles.beyondScroll
+            : startWhite
+            ? styles.startWhite
+            : "",
+        ].join(" ")}
+      >
+        <Link to="/">
+          <div className={styles.swiftIcon} />
+        </Link>
+        <div className={styles.headerLinks}>
+          {/*<div className={styles.dropDown}>
               <Link to="/">Home</Link>
               <div className={styles.dropDownMenu}>
                 <Link to="/tinkertanker">About Tinkertanker</Link>
                 <Link to="/programme-outline">Programme Outline</Link>
               </div>
             </div>*/}
-            {links.map((link) => <Link to={link.href}>{link.name}</Link>)}
-            <FontAwesomeIcon icon={["fas", "bars"]} onClick={() => setDrawerOpen(!isDrawerOpen)} />
-            <div className={styles.drawer} style={{ width: isDrawerOpen ? '100vw' : '0px' }}>
-              <a href="javascript:void(0)" className={styles.closeBtn} onClick={() => setDrawerOpen(!isDrawerOpen)}>&times;</a>
-              {links.map((link) => <Link to={link.href}>{link.name}</Link>)}
-            </div>
+          {links.map((link) => (
+            <Link to={link.href}>{link.name}</Link>
+          ))}
+          <FontAwesomeIcon
+            icon={["fas", "bars"]}
+            onClick={() => setDrawerOpen((c) => !c)}
+          />
+          <div
+            className={styles.drawer}
+            style={{ width: isDrawerOpen ? "100vw" : "0px" }}
+          >
+            <a
+              href="javascript:void(0)"
+              className={styles.closeBtn}
+              onClick={() => setDrawerOpen(!isDrawerOpen)}
+            >
+              &times;
+            </a>
+            {links.map((link) => (
+              <Link style={{whiteSpace: "nowrap"}} to={link.href}>{link.name}</Link>
+            ))}
           </div>
-        </header>
-    </>);
+        </div>
+      </header>
+    </>
+  );
 }
 
 export default Header;
